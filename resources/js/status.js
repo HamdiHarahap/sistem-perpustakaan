0d851b2e13298b5c6402b6f6082f50e56750c305
HamdiHarahap <hamdiharahap2005@gmail.com>
2025-05-10 12:57:53 +0700

    commit

--- resources/js/status.js
+++ resources/js/status.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("click", function (event) {
-        const button = event.target.closest(".btn-status");
-        if (!button) return;
-
-        if (button.textContent.trim() === "kembali") {
-            Swal.fire({
-                title: "Update Status",
-                text: "Tidak dapat mengubah status",
-                icon: "info",
-            });
-        } else {
-            event.preventDefault();
-            Swal.fire({
-                title: "Kamu yakin ingin mengubah status?",
-                text: "Status akan di ubah ke kembali",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Ya, ubah status",
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    button.closest("form").submit();
-                }
-            });
-        }
+    const buttons = document.querySelectorAll(".btn-status");
+    buttons.forEach((button) => {
+        button.addEventListener("click", function (event) {
+            if (button.textContent.trim() === "kembali") {
+                Swal.fire({
+                    title: "Update Status",
+                    text: "Tidak dapat mengubah status",
+                    icon: "info",
+                });
+            } else {
+                event.preventDefault();
+                Swal.fire({
+                    title: "Kamu yakin ingin mengubah status?",
+                    text: "Status akan di ubah ke kembali",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Ya, ubah status",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        this.closest("form").submit();
+                    }
+                });
+            }
+        });
     });
 });
